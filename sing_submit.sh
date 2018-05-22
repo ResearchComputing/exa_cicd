6f1de59e9bac0c68975dfc3e91d43f9536be2c77
AaronTHolt <holtat@colorado.edu>
2018-05-22 09:43:15 -0600

    Make image name dynamic, remove broken 'git log' argument

--- sing_submit.sh
+++ sing_submit.sh
@@ -4,21 +4,21 @@
 #SBATCH --account ucb1_summit1
 #SBATCH --time 04:00:00
 ##SBATCH --output normal.out
-#SBATCH --reservation bench-bandwidth
+##SBATCH --reservation bench-bandwidth
 
 ml singularity/2.4.2 gcc/6.1.0
 
-export MFIX=/app/mfix/build/mfix/mfix
+export MFIX=/app/mfix/build/mfix
 export WD=/scratch/summit/holtat/sing
-export IMAGE=/curc/tools/images/holtat/mfix_fulllatest.simg
-export MPIRUN=/projects/holtat/spack/opt/spack/linux-rhel7-x86_64/gcc-6.1.0/openmpi-2.1.0-4wsyjochpemaio57jlxq274utszh5are/bin/mpirun
+export IMAGE=/scratch/summit/holtat/singularity/holtat-mfix_full:latest.simg
+export MPIRUN=/projects/holtat/spack/opt/spack/linux-rhel7-x86_64/gcc-6.1.0/openmpi-2.1.2-foemyxg2vl7b3l57e7vhgqtlwggubj3a/bin/mpirun
 
 ## Formatting for output files
 ## Latest commit date, format: 2018-02-19 12:44:03 -0800
-singularity exec mfix.img bash -c "cd /app/mfix; git log -n 1 --pretty=format:'%ai' develop" > info.txt
+singularity exec $IMAGE bash -c "cd /app/mfix; git log -n 1 --pretty=format:'%ai'" > info.txt
 echo '' >> info.txt
 ## Shortened latest commit hash, format: b119a72
-singularity exec mfix.img bash -c "cd /app/mfix; git log -n 1 --pretty=format:'%h' develop" >> info.txt
+singularity exec $IMAGE bash -c "cd /app/mfix; git log -n 1 --pretty=format:'%h'" >> info.txt
 echo '' >> info.txt
 
 export DATE=$(sed '1q;d' info.txt | awk '{print $1;}')
