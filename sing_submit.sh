c2dcdf6e874939962ccde63821f9fd9857f4e942
AaronTHolt <holtat@colorado.edu>
2018-06-28 13:33:24 -0600

    Save metadata of each run to unique file

--- sing_submit.sh
+++ sing_submit.sh
@@ -20,18 +20,29 @@ echo '' >> info.txt
 ## Shortened latest commit hash, format: b119a72
 singularity exec $IMAGE bash -c "cd /app/mfix; git log -n 1 --pretty=format:'%h'" >> info.txt
 echo '' >> info.txt
+## Nodelist
+echo $SLURM_NODELIST >> info.txt
+## Modules
+ml 2>&1 | grep 1 >> info.txt
 
 export DATE=$(sed '1q;d' info.txt | awk '{print $1;}')
 export HASH=$(sed '2q;d' info.txt)
 echo $DATE
 echo $HASH
+echo $SLURM_NODELIST
+
+cp info.txt /projects/holtat/CICD/results/weak_scaling_small/metadata/${DATE}_${HASH}.txt
 
 for dir in {np_00001,np_00004,np_00008,np_00016,np_00024}; do
 
+    # Make dir if needed
     mkdir -p $WD/$dir
     cd $WD/$dir
     pwd
-    $MPIRUN -np 24 singularity exec $IMAGE bash -c "cd $WD/$dir; $MFIX inputs >> ${DATE}_${HASH}_${dir}"
+    # Get np from dir
+    np=${dir:(-5)}
+    np=$((10#$np))
+    $MPIRUN -np $np singularity exec $IMAGE bash -c "cd $WD/$dir; $MFIX inputs >> ${DATE}_${HASH}_${dir}"
 
 done
 
