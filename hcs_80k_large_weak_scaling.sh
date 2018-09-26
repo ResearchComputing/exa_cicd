e0657ab2a9cc95bffd0de2e8454cad7d26621fbe
AaronTHolt <holtat@colorado.edu>
2018-09-26 10:49:28 -0600

    Update account in batch scripts

--- hcs_80k_large_weak_scaling.sh
+++ hcs_80k_large_weak_scaling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes 9
 #SBATCH --exclusive
-#SBATCH --account ucb1_summit1
+#SBATCH --account ucb1_summit2
 #SBATCH --time 04:00:00
 ##SBATCH --output normal.out
 ##SBATCH --reservation bench-bandwidth
