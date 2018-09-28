2a4d9ae27d427746ae08fe1f2b223fe20f905708
AaronTHolt <holtat@colorado.edu>
2018-09-28 15:36:01 -0600

    Refactor init arguments

--- python_utils/scripts.py
+++ python_utils/scripts.py
@@ -2,20 +2,34 @@
 class ScriptUtils:
     """Create functions and variables for code reuse in mfix exa slurm scripts"""
 
-    def __init__(self, commit_hash):
-        """commit_hash = 7 character string, ex: 6b3f4ac"""
-        self.mfix = "/app/mfix/build/mfix/mfix"
-        self.base_working_dir = "/scratch/summit/holtat"
-        self.commit_hash = commit_hash
-        self.image_path = "/scratch/summit/holtat/singularity/holtat-mfix_full:develop_" + self.commit_hash + ".simg"
-        self.mpirun = "/projects/holtat/spack/opt/spack/linux-rhel7-x86_64/gcc-6.1.0/openmpi-2.1.2-foemyxg2vl7b3l57e7vhgqtlwggubj3a/bin/mpirun"
-        self.modules = ["singularity/2.5.2", "gcc/6.1.0"] # Lmod modules to load
-        self.mpi_processes = None # np list to be handed to mpirun, ex: [1, 8, 27, 64]
+    def __init__(self, commit_hash,
+            mfix="/app/mfix/build/mfix/mfix",
+            base_working_dir="/scratch/summit/holtat",
+            image_prefix="/scratch/summit/holtat/singularity/holtat-mfix_full:develop_",
+            image_suffix=".simg"
+            mpirun="/projects/holtat/spack/opt/spack/linux-rhel7-x86_64/gcc-6.1.0/openmpi-2.1.2-foemyxg2vl7b3l57e7vhgqtlwggubj3a/bin/mpirun",
+            mpi_processes=None,
+            modules=["singularity/2.5.2", "gcc/6.1.0"],
+            account="ucb1_summit2",
+            exclusive=True,
+            nodes=None,
+            time_limit=None):
+        self.commit_hash = commit_hash # 7 character string, ex: 6b3f4ac
+        self.mfix = mfix # mfix executable path
+        self.base_working_dir = base_working_dir
+        ## Singularity image path = prefix+hash+suffix
+        # ex: "/scratch/summit/holtat/singularity/holtat-mfix_full:develop_6b3f4ac.simg"
+        self.image_prefix = image_prefix
+        self.image_suffix = image_suffix
+        self.image_path = self.image_prefix + self.commit_hash + self.image_suffix
+        self.mpirun = mpirun # mpirun executable
+        self.modules = modules # Lmod modules to load
+        self.mpi_processes = mpi_processes # np list to be handed to mpirun, ex: [1, 8, 27, 64]
         ## Slurm script options
-        self.exclusive = True # Reserve full node?
-        self.nodes = None # Number of nodes reserve
-        self.account = "ucb1_summit2" # Slurm account to submit to
-        self.time_limit = None # Max wall time
+        self.account = account # Slurm account to submit to
+        self.exclusive = exclusive # Reserve full node?
+        self.nodes = nodes # Number of nodes reserve
+        self.time_limit = time_limit # Max wall time
 
     def get_commit_date(self):
         """Get date of commit hash"""
