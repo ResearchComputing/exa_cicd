467d44b223b572f45bcfb7729902d66e7a1b11dc
AaronTHolt <holtat@colorado.edu>
2018-10-03 13:24:27 -0600

    Improve parser formatting

--- python_utils/submit_mfix.py
+++ python_utils/submit_mfix.py
@@ -3,12 +3,36 @@ from scripts import ScriptUtils, BatchTemplate
 
 parser = argparse.ArgumentParser(description='Inputs for ScriptUtils')
 ## Required args
-parser.add_argument('--commit-hash', dest='commit_hash', type=str, help='Git commit hash on mfix-exa develop branch', required=True)
-parser.add_argument('--results-dir', dest='results_dir', type=str, help='Base directory to permanently store results' required=True)
-parser.add_argument('--working-dir', dest='working_dir', type=str, help='High performance directory to initially output results to' required=True)
-parser.add_argument('--np-list', dest='np_list', type=list, help='Run mfix exa with each np in np_list' required=True)
-parser.add_argument('--nodes', dest='nodes', type=int, help='Number of nodes to run on', required=True)
-parser.add_argument('--time-limit', dest='time_limit', type='int', help='Max walltime in hours', required=True)
+parser.add_argument('--commit-hash',
+    dest='commit_hash',
+    type=str,
+    help='Git commit hash on mfix-exa develop branch',
+    required=True)
+parser.add_argument('--results-dir',
+    dest='results_dir',
+    type=str,
+    help='Base directory to permanently store results',
+    required=True)
+parser.add_argument('--working-dir',
+    dest='working_dir',
+    type=str,
+    help='High performance directory to initially output results to',
+    required=True)
+parser.add_argument('--np-list',
+    dest='np_list',
+    type=list,
+    help='Run mfix exa with each np in np_list',
+    required=True)
+parser.add_argument('--nodes',
+    dest='nodes',
+    type=int,
+    help='Number of nodes to run on',
+    required=True)
+parser.add_argument('--time-limit',
+    dest='time_limit',
+    type='int',
+    help='Max walltime in hours',
+    required=True)
 ## Optional args
 parser.add_argument('--account',
     dest='account',
