60748f22b56667e22967f402e6dd750fd7444901
Aaron Holt <aaron.holt@colorado.edu>
2019-08-14 10:03:32 -0600

    Add inputs to messagebuilder class Print results at end for debug

--- Elasticsearch/output_to_es.py
+++ Elasticsearch/output_to_es.py
@@ -2,59 +2,60 @@
 Takes MFiX-Exa output from the tiny profiler and stores it in elasticsearch.
 """
 
+import argparse
 import datetime
 import subprocess
 
 from elasticsearch_utils import get_elasticsearch_client
 
-# Variables I have in slurm script:
-#{DATE} ${HASH} ${dir} {BRANCH} {COMMIT} {IMAGE}
-#IMAGE=/scratch/summit/holtat/singularity/mfix-exa_${BRANCH}_${COMMIT}.sif
-# Can get def file from "singularity inspect --deffile image_path"
+parser = argparse.ArgumentParser(description='Inputs for process class')
+parser.add_argument('--work-dir', dest='work_dir', type=str, help='Directory where data directories live')
+parser.add_argument('--np', dest='np', type=str, help='length 4 string representing number of processes used')
+parser.add_argument('--commit-date', dest='commit_date', type=str, help='Day of latest commit yyy-mm-dd')
+parser.add_argument('--git-hash', dest='git_hash', type=str, help='Shortened mfix-exa git_hash (length 7)')
+parser.add_argument('--git-branch', dest='git_branch', type=str, help='Shortened mfix-exa gitbranch (length 7)')
+parser.add_argument('--image-path', dest='image_path', type=str, help='Singularity image path')
+parser.add_argument('--type', dest='type', type=str, default=None, help='Special argument passed to exa (None, adapt)')
+args = parser.parse_args()
 
-def get_output_filenames(basedir, runtype, np, rundate, githash, gitbranch,
-                            type, singularity_dir):
+def get_output_filenames(work_dir, np, commit_date, git_hash, git_branch, type):
     '''Inputs:
-    basedir: String base output directory filepath (ex: /scratch/summit/$USER)
-    runtype: String directory name of the run (eg: hcs_200k_ws)
+    work_dir: String base output directory filepath (ex: /scratch/summit/$USER/hcs_200k_ws)
     np: 4 digit string representing number of processes used
-    githash: shortened mfix-exa githash, string of length 7
-    rundate: string of the rundate yyyy-mm-dd
+    git_hash: shortened mfix-exa githash, string of length 7
+    commit_date: string of the commit_date yyyy-mm-dd
     type: string of the special parameters used (ex: adapt)
-    singularity_dir: string directory name where singularity images are found
-
+    singularity_image_path: string filepath where image is found
     Returns the mfix output filepath, the metadata filepath, and the
     Singularity image filepath '''
 
-    if not basedir[-1] == '/':
-        basedir = basedir + '/'
-    rundir = basedir + runtype + '/'
-    output_filepath = rundir + np + '/' + rundate + '_' + \
-                          githash + '_np_' + np
+    if not work_dir[-1] == '/':
+        work_dir = work_dir + '/'
+    output_filepath = work_dir + 'np_' + np + '/' + commit_date + '_' + \
+                          git_hash + '_np_' + np
     if type:
         output_filepath += '_' + type
 
-    metadata_filepath = rundir + gitbranch + '_' + githash + '_info.txt'
+    metadata_filepath = work_dir + git_branch + '_' + git_hash + '_info.txt'
 
-    singularity_image_filepath = singularity_dir + 'mfix-exa_' + gitbranch + \
-                                 '_' + githash + '.sif'
+    # singularity_image_filepath = singularity_dir + 'mfix-exa_' + git_branch + \
+    #                              '_' + git_hash + '.sif'
 
-    return output_filepath, metadata_file, singularity_image_file
+    return output_filepath, metadata_filepath
 
-def get_input_filepaths(basedir, runtype, np_list, type):
+def get_input_filepaths(work_dir, np, type):
     '''Inputs:
-    basedir: String base output directory filepath (ex: /scratch/summit/$USER)
-    runtype: String directory name of the run (eg: hcs_200k_ws)
-    np_list: list of 4 digit strings representing number of processes used
+    work_dir: String base output directory filepath (ex: /scratch/summit/$USER/hcs_200k_ws)
+    np: 4 digit string representing number of processes used
     type: string of the special parameters used (ex: adapt)
     Outputs:
     mfixdat_filepath: string
     inputs_filepath: string
     '''
-    if not basedir[-1] == '/':
-        basedir = basedir + '/'
+    if not work_dir[-1] == '/':
+        work_dir = work_dir + '/'
 
-    output_dir = basedir + runtype + '/' + np + '/'
+    output_dir = work_dir + 'np_' + np + '/'
     mfixdat_filepath = output_dir + 'mfix.dat'
     inputs_filepath = output_dir + 'inputs'
 
@@ -64,10 +65,19 @@ def get_input_filepaths(basedir, runtype, np_list, type):
     return mfixdat_filepath, inputs_filepath
 
 
-
 class MfixElasticsearchMessageBuilder:
 
-    def __init__(self):
+    def __init__(self,
+                 mfix_output_filepath,
+                 metadata_filepath,
+                 mfixdat_filepath,
+                 inputs_filepath,
+                 singularity_image_filepath):
+        self.mfix_output_filepath = mfix_output_filepath
+        self.metadata_filepath = metadata_filepath
+        self.mfixdat_filepath = mfixdat_filepath
+        self.inputs_filepath = inputs_filepath
+        self.singularity_image_filepath = singularity_image_filepath
         self.function_list = ["calc_particle_collisions()",
             "MLNodeLaplacian::Fsmooth()",
             "FillBoundary_nowait()",
@@ -80,9 +90,7 @@ class MfixElasticsearchMessageBuilder:
             "NeighborParticleContainer::fillNeighborsMPI",
             "ParticleContainer::RedistributeMPI()",
             "mfix_dem::EvolveParticles_tstepadapt()",]
-
         self.message = {}
-
         self.elasticsearch_client = get_elasticsearch_client()
 
     def index_mfix_message(self):
@@ -93,22 +101,13 @@ class MfixElasticsearchMessageBuilder:
             print(res['result'])
 
     def build_mfix_elasticsearch_message(self):
-        mfix_output_filepath = '/home/aaron/hcs_200k_ws/np_0027/2019-07-05_2437f2c_np_0027_adapt'
-        self.get_function_times_from_file(mfix_output_filepath)
-        self.get_np(mfix_output_filepath)
-
-        job_info_file = '/home/aaron/hcs_200k_ws/phase2-develop_2437f2c_info.txt'
-        self.get_git_info(job_info_file)
-        self.get_slurm_job_info(job_info_file)
-
-        singularity_def_file = "/home/aaron/exa_cicd/def_files/mfix.def"
-        self.get_singularity_def_file(singularity_def_file)
-
-        inputs_file = "/home/aaron/hcs_200k_ws/np_0001/inputs"
-        self.get_inputs_file(inputs_file)
-
-        mfix_dat_file = "/home/aaron/hcs_200k_ws/np_0001/mfix.dat"
-        self.get_mfix_dat_file(mfix_dat_file)
+        self.get_function_times_from_file(self.mfix_output_filepath)
+        self.get_np(self.mfix_output_filepath)
+        self.get_git_info(self.metadata_filepath)
+        self.get_slurm_job_info(self.metadata_filepath)
+        self.get_singularity_def_file_from_image(self.singularity_image_filepath)
+        self.get_inputs_file(self.inputs_filepath)
+        self.get_mfix_dat_file(self.mfixdat_filepath)
 
     def get_inputs_file(self, filepath):
         with open(filepath, 'r') as file:
@@ -196,6 +195,24 @@ class MfixElasticsearchMessageBuilder:
         self.message['date'] = runtime.strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
-builder = MfixElasticsearchMessageBuilder()
+output_filepath, metadata_file = get_output_filenames(args.work_dir,
+        args.np, args.commit_date, args.git_hash, args.git_branch, args.type)
+
+mfixdat_filepath, inputs_filepath = get_input_filepaths(args.work_dir, args.np, args.type)
+
+builder = MfixElasticsearchMessageBuilder(output_filepath, metadata_file,
+                            mfixdat_filepath, inputs_filepath, args.image_path)
 builder.build_mfix_elasticsearch_message()
-builder.index_mfix_message()
+# print(builder.message)
+
+for key, item in builder.message.items():
+    print(key, item)
+## Index results into elasticsearch
+# builder.index_mfix_message()
+
+## Testing items
+# print(args)
+# python output_to_es.py --work-dir /home/aaron/hcs_200k_ws --np 0008 --commit-date 2019-07-05 \
+# --git-hash 2437f2c --git-branch phase2-develop --image-path /home/aaron/hcs_200k_ws/mfix-exa_phase2-develop_2437f2c.sif \
+# --type adapt
+# print(output_filepath, metadata_file, mfixdat_filepath, inputs_filepath, args.image_path)
