edc843fb9ed2b17ae25e3ceb932ad91e92579298
Aaron Holt <aaron.holt@colorado.edu>
2019-08-15 08:49:00 -0600

    Uncomment mfix run commands

--- batch_scripts/hcs_200k_ws.sh
+++ batch_scripts/hcs_200k_ws.sh
@@ -62,8 +62,8 @@ for dir in {np_0001,np_0008,np_0027}; do
     np=$((10#$np))
 
     # Run default then timestepping
-    #$MPIRUN -np $np singularity exec $IMAGE bash -c "$MFIX inputs >> ${DATE}_${HASH}_${dir}"
-    #$MPIRUN -np $np singularity exec $IMAGE bash -c "$MFIX inputs_adapt >> ${DATE}_${HASH}_${dir}_adapt"
+    $MPIRUN -np $np singularity exec $IMAGE bash -c "$MFIX inputs >> ${DATE}_${HASH}_${dir}"
+    $MPIRUN -np $np singularity exec $IMAGE bash -c "$MFIX inputs_adapt >> ${DATE}_${HASH}_${dir}_adapt"
 
 ##mfix.use_tstepadapt=0
     #Consider mpirun -np $np --map-by node ...
