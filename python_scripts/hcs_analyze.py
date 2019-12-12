b66e99834da384eae11a8a9a84f993d4069c96dc
Aaron Holt <aaron.holt@colorado.edu>
2019-12-12 09:57:12 -0700

    Fix tabs and spaces issue

--- python_scripts/hcs_analyze.py
+++ python_scripts/hcs_analyze.py
@@ -1,5 +1,6 @@
 # Original code by: Hari Sitaraman
-### Use as `python hcs_analyze.py "plt*" <no: of particles>
+### Use as `python hcs_analyze.py
+### -pfp "plt*" -np 5050 -e 0.8 -T0 1000.0 -diap 0.01 --rho-s 1.0 --rho-g 0.001 --mu-g 0.0002 --ld 64
 ### It takes the glob pattern of the plot files and the number of particles as argument
 
 import argparse
@@ -10,91 +11,93 @@ from sys import argv
 import glob
 
 parser = argparse.ArgumentParser(description='Homogeneous cooling system property inputs')
-parser.add_arguement('-pfp', '--plot-file-pattern', dest='pfp', type=str, help='Plot file pattern prefix glob (ex: plt*)')
-parser.add_arguement('-np', '--number-particles', dest='npart', type=float, help='number of particles')
-parser.add_arguement('-e', '--restitution-coefficient', dest='e', type=float, help='particle-particle restitution coefficient')
-parser.add_argument('-T0', '--granular-energy', dest='T0', type=float, help='granular energy (cm^2/s^2)')
-parser.add_argument('-diap', '--particle-diameter', dest='diap', type=float, help='particle_diameter (cm)')
-parser.add_argument('--rho-s', dest='rho_s', type=float, help='solids density (g/cm^3)')
-parser.add_argument('--rho-g', dest='rho_g', type=float, help='gas density (g/cm^3)')
-parser.add_argument('--mu-g', dest='mu_g', type=float, help='viscosity (g/cm*s)')
-parser.add_argument('--ld', dest='ld', type=float, help='ratio of box size to particle size')
+parser.add_argument('-pfp', '--plot-file-pattern', dest='pfp', type=str, required=True, help='Plot file pattern prefix glob (ex: plt*)')
+parser.add_argument('-np', '--number-particles', dest='npart', type=int, required=True, help='number of particles')
+parser.add_argument('-e', '--restitution-coefficient', dest='e', type=float, required=True, help='particle-particle restitution coefficient')
+parser.add_argument('-T0', '--granular-energy', dest='T0', type=float, required=True, help='granular energy (cm^2/s^2)')
+parser.add_argument('-diap', '--particle-diameter', dest='diap', type=float, required=True, help='particle_diameter (cm)')
+parser.add_argument('--rho-s', dest='rho_s', type=float, required=True, help='solids density (g/cm^3)')
+parser.add_argument('--rho-g', dest='rho_g', type=float, required=True, help='gas density (g/cm^3)')
+parser.add_argument('--mu-g', dest='mu_g', type=float, required=True, help='viscosity (g/cm*s)')
+parser.add_argument('--ld', dest='ld', type=float, required=True, help='ratio of box size to particle size')
 args = parser.parse_args()
 
 def get_analytic_soln(npart, e, T0, diap, rho_s, rho_g, mu_g, Ld):
 #---------------------------------------------------------------------------------------------------------
 #           % Required user input
 #---------------------------------------------------------------------------------------------------------
-	Np   = npart;
-	# e    = 0.8; #restitution coefficient
-	# T0   = 1000.0; #granular energy (cm^2/s^2)
-	# diap = 0.01; #particle diameter (cm)
-	# rho_s = 1.0; #density solid (g/cm^3)
-	# rho_g = 1.0e-3; #density gas (g/cm^3)
-	# mu_g = 2.0e-4; #viscosity ( g/(cm*s) )
-	# Ld   = 64.0; # Ratio of box size to particle size
+    Np   = npart;
+    print(e, T0, diap, rho_s, rho_g, mu_g, Ld)
+    # e    = 0.8; #restitution coefficient
+    # T0   = 1000.0; #granular energy (cm^2/s^2)
+    # diap = 0.01; #particle diameter (cm)
+    # rho_s = 1.0; #density solid (g/cm^3)
+    # rho_g = 1.0e-3; #density gas (g/cm^3)
+    # mu_g = 2.0e-4; #viscosity ( g/(cm*s) )
+    # Ld   = 64.0; # Ratio of box size to particle size
+    # print(e, T0, diap, rho_s, rho_g, mu_g, Ld)
 #---------------------------------------------------------------------------------------------------------
 # Dependent Variables
-	phi   = Np*(np.pi/6.0)/Ld**3;
-	nu_g  = mu_g/rho_g;
-	m     = rho_s*(np.pi/6.0)*diap**3;
-	r_rho = 1000.0;
-	L     = Ld*diap;
-	ReT0  = rho_g*diap*np.sqrt(T0)/mu_g;
+    phi   = Np*(np.pi/6.0)/Ld**3;
+    nu_g  = mu_g/rho_g;
+    m     = rho_s*(np.pi/6.0)*diap**3;
+    r_rho = 1000.0;
+    L     = Ld*diap;
+    ReT0  = rho_g*diap*np.sqrt(T0)/mu_g;
 #---------------------------------------------------------------------------------------------------------
 #           % comment above out if called from dem_data_cruncher
 #---------------------------------------------------------------------------------------------------------
 #
 #
 # T-independent variables :
-	chi      = (1.0-phi/2.0)/(1-phi)**3;
-	mu20     = np.sqrt(2.0*np.pi)*chi*(1-e**2);
-	mu40     = (9.0/2.0+e**2)*mu20;
-	mu41     = ((3.0/32.0)*(69.0+10.0*e**2)+2.0/(1-e))*mu20;
-	xistar   = 0.0;
-	a2       = (5.0*mu20 - mu40)/(mu41 - 5.0*((19.0/16.0)*mu20 - (3.0/2.0)*xistar));
+    chi      = (1.0-phi/2.0)/(1-phi)**3;
+    mu20     = np.sqrt(2.0*np.pi)*chi*(1-e**2);
+    mu40     = (9.0/2.0+e**2)*mu20;
+    mu41     = ((3.0/32.0)*(69.0+10.0*e**2)+2.0/(1-e))*mu20;
+    xistar   = 0.0;
+    a2       = (5.0*mu20 - mu40)/(mu41 - 5.0*((19.0/16.0)*mu20 - (3.0/2.0)*xistar));
 #
-	zeta0hat = (8.0*phi)/(np.sqrt(np.pi)*diap)*chi*(1.0 - e**2)*(1.0 + 3.0*a2/16.0);
-	Rdiss0 = 1.0+3.0*np.sqrt(phi/2.0)+(135.0/64.0)*phi*np.log(phi)+\
-	11.26*phi*(1.0-5.1*phi+16.57*phi**2-21.77*phi**3)-phi*chi*np.log(0.01);
-	#%Rdrag  = (1+3*np.sqrt(phi/2)+(135/64)*phi*np.log(phi)+17.14*phi)/(1+0.681*phi-8.48*phi**2+8.16*phi**3);
-	#%chiMA  = (1+2.5*phi+4.5094*phi**2+4.515439*phi**3)/(1-(phi/0.64356)**3)**0.678021;
-	#%Sstar  = Rdrag**2/(chiMA*(1 + 3.5*np.sqrt(phi)+5.9*phi));
-	Kofphi = (0.096 + 0.142*phi**0.212)/(1 - phi)**4.454;
+    zeta0hat = (8.0*phi)/(np.sqrt(np.pi)*diap)*chi*(1.0 - e**2)*(1.0 + 3.0*a2/16.0);
+    Rdiss0 = 1.0+3.0*np.sqrt(phi/2.0)+(135.0/64.0)*phi*np.log(phi)+\
+    11.26*phi*(1.0-5.1*phi+16.57*phi**2-21.77*phi**3)-phi*chi*np.log(0.01);
+    #%Rdrag  = (1+3*np.sqrt(phi/2)+(135/64)*phi*np.log(phi)+17.14*phi)/(1+0.681*phi-8.48*phi**2+8.16*phi**3);
+    #%chiMA  = (1+2.5*phi+4.5094*phi**2+4.515439*phi**3)/(1-(phi/0.64356)**3)**0.678021;
+    #%Sstar  = Rdrag**2/(chiMA*(1 + 3.5*np.sqrt(phi)+5.9*phi));
+    Kofphi = (0.096 + 0.142*phi**0.212)/(1 - phi)**4.454;
 #
 
-	gammahat = 3.0*np.pi*mu_g*diap;
-	gamma    = gammahat*Rdiss0;
-	gammaK   = gammahat*(rho_g*diap/mu_g)*Kofphi;
+    gammahat = 3.0*np.pi*mu_g*diap;
+    gamma    = gammahat*Rdiss0;
+    gammaK   = gammahat*(rho_g*diap/mu_g)*Kofphi;
 
 #solution consts
-	A = zeta0hat + 2.0*gammaK/m;
-	B = 2.0*gamma/m;
+    A = zeta0hat + 2.0*gammaK/m;
+    B = 2.0*gamma/m;
 
-	n = 800;
-	xs = np.zeros(n);
-	x  = np.zeros(n);
-	y  = np.zeros(n);
-	minexp=-1.0
-	maxexp=2.5
-	delexp=(maxexp-minexp)/(n-1)
-	for i in range(n):
-    		xs[i]=10.0**(minexp+i*delexp)
+    n = 800;
+    xs = np.zeros(n);
+    x  = np.zeros(n);
+    y  = np.zeros(n);
+    minexp=-1.0
+    maxexp=2.5
+    delexp=(maxexp-minexp)/(n-1)
+    for i in range(n):
+        xs[i]=10.0**(minexp+i*delexp)
 
-	for i in range(n):
-   		x[i] = xs[i]*diap/np.sqrt(T0);
-    		y[i] = 1.0/(np.exp(B*x[i]/2.0) + (A/B)*np.sqrt(T0)*(np.exp(B*x[i]/2) - 1))**2;
+    for i in range(n):
+        x[i] = xs[i]*diap/np.sqrt(T0);
+        y[i] = 1.0/(np.exp(B*x[i]/2.0) + (A/B)*np.sqrt(T0)*(np.exp(B*x[i]/2) - 1))**2;
 
     #% output: x = t (time), xs = t* = t*np.sqrt(T0)/diap, y = T(t*)/T0 (T0 = T(t* = 0))
     #fprintf(1,'%20.12e\t%20.12e\n',xs,y)
 
-	outfile=open("analytic_soln.dat","w")
+    outfile=open("analytic_soln.dat","w")
 
-	for i in range(n):
-    		outfile.write("%e\t%e\n"%(xs[i],y[i]))
+    for i in range(n):
+            outfile.write("%e\t%e\n"%(xs[i],y[i]))
 
-	outfile.close()
-	return(xs,y,diap/np.sqrt(T0),T0)
+    outfile.close()
+    return(xs,y,diap/np.sqrt(T0),T0)
 
 class AMReXParticleHeader(object):
     '''
@@ -178,69 +181,68 @@ def get_time_stamp(fldfname):
 
 def get_computed_soln(fname_list,tscale,v2scale,ptype="particles"):
 
-	outfile=open("vel_computed.dat","w")
+    outfile=open("vel_computed.dat","w")
 
-        m2_to_cm2 = 10000.0
-	nondim_temp=np.array([])
-	nondim_time=np.array([])
+    m2_to_cm2 = 10000.0
+    nondim_temp=np.array([])
+    nondim_time=np.array([])
 
-    	for fn_i, fn in enumerate(fname_list):
+    for fn_i, fn in enumerate(fname_list):
 
-		# print(fn)
-    		time=get_time_stamp(fn)
-    		base_fn = fn + "/" + ptype
-    		header = AMReXParticleHeader(base_fn + "/Header")
+        time=get_time_stamp(fn)
+        base_fn = fn + "/" + ptype
+        header = AMReXParticleHeader(base_fn + "/Header")
 
 
-    		idtype = "(%d,)i4" % header.num_int
-    		if header.real_type == np.float64:
-        		fdtype = "(%d,)f8" % header.num_real
-    		elif header.real_type == np.float32:
-        		fdtype = "(%d,)f4" % header.num_real
+        idtype = "(%d,)i4" % header.num_int
+        if header.real_type == np.float64:
+            fdtype = "(%d,)f8" % header.num_real
+        elif header.real_type == np.float32:
+            fdtype = "(%d,)f4" % header.num_real
 
-    		idata = np.empty((header.num_particles, header.num_int ))
-    		rdata = np.empty((header.num_particles, header.num_real))
+        idata = np.empty((header.num_particles, header.num_int ))
+        rdata = np.empty((header.num_particles, header.num_real))
 
-    		ip = 0
-    		for lvl, level_grids in enumerate(header.grids):
-        		for (which, count, where) in level_grids:
-            			if count == 0: continue
-            			fn = base_fn + "/Level_%d/DATA_%05d" % (lvl, which)
+        ip = 0
+        for lvl, level_grids in enumerate(header.grids):
+            for (which, count, where) in level_grids:
+                    if count == 0: continue
+                    fn = base_fn + "/Level_%d/DATA_%05d" % (lvl, which)
 
-            			with open(fn, 'rb') as f:
-                			f.seek(where)
-                			if header.is_checkpoint:
-                    				ints   = np.fromfile(f, dtype = idtype, count=count)
-                    				idata[ip:ip+count] = ints
+                    with open(fn, 'rb') as f:
+                        f.seek(where)
+                        if header.is_checkpoint:
+                                ints   = np.fromfile(f, dtype = idtype, count=count)
+                                idata[ip:ip+count] = ints
 
-                			floats = np.fromfile(f, dtype = fdtype, count=count)
-                			rdata[ip:ip+count] = floats
-            			ip += count
+                        floats = np.fromfile(f, dtype = fdtype, count=count)
+                        rdata[ip:ip+count] = floats
+                    ip += count
 
-    		vx=rdata[:,8]
-    		vy=rdata[:,9]
-    		vz=rdata[:,10]
+        vx=rdata[:,8]
+        vy=rdata[:,9]
+        vz=rdata[:,10]
 
-    		vxm=np.mean(vx)
-    		vym=np.mean(vy)
-    		vzm=np.mean(vz)
+        vxm=np.mean(vx)
+        vym=np.mean(vy)
+        vzm=np.mean(vz)
 
-    		print(time, np.mean(vxm+vym+vzm))
+        print(time, np.mean(vxm+vym+vzm))
 
-    		pecvel_x=vx-vxm
-    		pecvel_y=vy-vym
-    		pecvel_z=vz-vzm
+        pecvel_x=vx-vxm
+        pecvel_y=vy-vym
+        pecvel_z=vz-vzm
 
-    		pecvel_speed2 = pecvel_x**2+pecvel_y**2+pecvel_z**2
+        pecvel_speed2 = pecvel_x**2+pecvel_y**2+pecvel_z**2
 
-                ndtime=time/tscale
-                ndtemp=0.33333*np.mean(pecvel_speed2)/v2scale*m2_to_cm2
+        ndtime=time/tscale
+        ndtemp=0.33333*np.mean(pecvel_speed2)/v2scale*m2_to_cm2
 
-		nondim_time = np.append(nondim_time,ndtime)
-		nondim_temp = np.append(nondim_temp,ndtemp)
-		outfile.write("%e\t%e\n"%(ndtime,ndtemp))
+        nondim_time = np.append(nondim_time,ndtime)
+        nondim_temp = np.append(nondim_temp,ndtemp)
+        outfile.write("%e\t%e\n"%(ndtime,ndtemp))
 
-	return(nondim_time,nondim_temp);
+    return(nondim_time,nondim_temp);
 
 #main
 font={'family':'Helvetica', 'size':'16'}
@@ -253,12 +255,13 @@ plot_data=True
 # npart=int(argv[2])
 # part_fn_list = glob.glob(part_fn_pattern)
 # part_fn_list.sort()
+part_fn_list = glob.glob(args.pfp)
+print(part_fn_list.sort())
+part_fn_list.sort()
 
 (a_time,a_temp,tscale,v2scale)=get_analytic_soln(args.npart, args.e, args.T0, args.diap, args.rho_s,
-                                      			  args.rho_g, args.mu_g, args.ld)
+                                                    args.rho_g, args.mu_g, args.ld)
 
-part_fn_list = glob.glob(args.pfp)
-part_fn_list.sort()
 (c_time,c_temp) = get_computed_soln(part_fn_list,tscale,v2scale)
 
 if(plot_data):
@@ -270,8 +273,8 @@ if(plot_data):
     plt.ylabel("Non-dimensional temperature $(T/T_0)$")
     plt.legend(loc="best")
     plt.tight_layout()
-    plt.savefig("haffs_law.png")
-    #plt.show()
+    # plt.savefig("haffs_law.png")
+    plt.show()
 
 
 #plt00350
