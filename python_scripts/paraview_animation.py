785fd5079a5fee776a181c76c65ffda72bc21a36
Aaron Holt <aaron.holt@colorado.edu>
2020-06-24 15:10:54 -0600

    Add fluidbed call, remove prints

--- python_scripts/paraview_animation.py
+++ python_scripts/paraview_animation.py
@@ -14,8 +14,6 @@ def LoadAmrexParticleFiles(
             index_step,
             camera_focal_point):
     filenames = [file_prefix + str(index).zfill(5) for index in range(low_index, high_index, index_step)]
-    # for i in range(low_index, high_index, index_step):
-    #     filenames.append
 
     reader = AMReXBoxLibParticlesReader(FileNames=filenames, ParticleType="particles")
     Show(reader)
@@ -23,27 +21,37 @@ def LoadAmrexParticleFiles(
     #print(dp.Representation, dp.GetProperty("Representation").Available)
     dp = GetDisplayProperties()
     dp.Representation = 'Point Gaussian'
-    for item in vars(dp)['_Proxy__Properties'].items():
-        print(item)
+    # for item in vars(dp)['_Proxy__Properties'].items():
+    #     print(item)
     # print(vars(dp))
 
     Render()
     camera = GetActiveCamera()
     camera.SetFocalPoint(camera_focal_point[0],camera_focal_point[1],camera_focal_point[2])
-    # Animate over all time steps. Note that we are not passing the optional
-    # 3rd argument here. If you pass a filename as the 3rd argument,
-    # AnimateReader will create a movie.
+    # Animate over all time steps.
     AnimateReader(reader)
     # Save the animation to an avi file
     SaveAnimation(filename=outfile)
 
 
 
+# LoadAmrexParticleFiles(
+#             outfile="hcs.avi",
+#             file_prefix="/home/aaron/projects/mfix_work/vis/hcs_5k/np_0027/adapt",
+#             low_index=0,
+#             high_index=925,
+#             index_step=25,
+#             camera_focal_point=[0.0095, 0.0095, 0.0095]
+#             )
+
+
+# Need to rotate axis so x is "up"
 LoadAmrexParticleFiles(
             outfile="flubed.avi",
-            file_prefix="/home/aaron/projects/mfix_work/vis/hcs_5k/np_0027/adapt",
+            file_prefix="/home/aaron/projects/mfix_work/vis/fluid_bed/adapt",
             low_index=0,
-            high_index=925,
-            index_step=25,
-            camera_focal_point=[0.0095, 0.0095, 0.0095]
+            high_index=10000,
+            index_step=500,
+            # camera_focal_point=[0,0,0]
+            camera_focal_point=[0.025, 0, 0]
             )
